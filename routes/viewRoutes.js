07b7e2f5d442d620386738a361131a0d04d70120
Viko-53 <vikobranevski93@gmail.com>
2024-11-12 23:10:33 +0100

    making stripe with webhooks

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,7 +8,7 @@ const {
   updateUserData
 } = require('../controllers/viewsController');
 const { isLoggedIn, protect } = require('../controllers/authController');
-const { createBookingCheckout } = require('../controllers/bookingController');
+// const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -18,7 +18,8 @@ router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
 
-router.get('/my-tours', createBookingCheckout, protect, getMyTours);
+// router.get('/my-tours', createBookingCheckout, protect, getMyTours);
+router.get('/my-tours', protect, getMyTours);
 
 router.post('/submit-user-data', protect, updateUserData);
 
