59a1f7988487f6e32a216ee212bc1b004e624c08
Viko-53 <vikobranevski93@gmail.com>
2024-12-02 16:45:47 +0100

    login fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
@@ -22,8 +21,6 @@ const { webhookCheckout } = require('./controllers/bookingController');
 
 const app = express();
 
-app.enable('trust proxy');
-
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -43,15 +40,6 @@ app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-// limit requests from same IP
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
-});
-
-app.use('/api', limiter);
 
 app.post(
   '/webhook-checkout',
